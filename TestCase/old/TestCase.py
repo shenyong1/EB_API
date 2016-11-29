cd0c4e523c9ccec1da06e626878fc35ec9bf7aba
Beyondhost-PC\Beyondhost <swang@beyondh.com>
2016-11-29 08:59:25 +0800

    TestCAse

--- TestCase/old/TestCase.py
+++ TestCase/old/TestCase.py
@@ -5,33 +5,27 @@ class UseTest():
         self.name = name
 
     def Setup(self):
+        print "Test Start"
+
+
+    def Test(self):
         RoomID=Add_RoomType(Add_RoomType_url,Room['RoomTypeName'],Room['RoomNumber'])
         result = RoomID[4]
         if (result != True):
-#             logging.error("%s", status[result])
             return result
-        logging.info("%s Setup Phase Done", self.name)
-        return result
-
-    def Test(self):
-        result = Search_All_RoomType(Search_All_RoomType_url,self.RoomID[0],self.RoomID[1])
+        
+        result = Search_All_RoomType(Search_All_RoomType_url,RoomID[0],RoomID[1])
         if (result != True):
-#             logging.error("%s", status[result])
             return result
-#         result = Search_All_RoomType(Search_All_RoomType_url,self.RoomID[0],self.RoomID[1])
-#         if (result != True):
-#             logging.error("%s", status[result])
-#             return result
-        logging.info("%s Test Phase Done", self.name)
-        return result
-
-    def CleanUp(self):
+        
         result = Del_RoomType(Del_RoomType_url, RoomID[2])
         if (result != True):
-#             logging.error("%s", status[result])
             return result
-        logging.info("%s CleanUp Phase Done", self.name)
-        return result
+
+
+    def CleanUp(self):
+        print "Test End"
+
 
 
 
