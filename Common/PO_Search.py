689a5ec2f55895c181edec4cc6abfaf5447d211e
Beyondhost-PC\Beyondhost <swang@beyondh.com>
2016-12-07 18:13:28 +0800

    no message

--- Common/PO_Search.py
+++ Common/PO_Search.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from Config.EB_API_Config import *
 from nose.config import flag
+from string import lower
 
 
 def CommonMoudle(name,TotalAmount,length):
@@ -12,11 +14,44 @@ def CommonMoudle(name,TotalAmount,length):
             return False
     else:
         print "%s JSOM_Data is %s.Date:%s"%(name,TotalAmount,today)
+        
+def RoomTypeName(RoomTypeName):
+    sql="SELECT * FROM iPms.RoomType where RoomTypeName='%s'"%RoomTypeName
+#     print sql
+    curs = conn.cursor()
+    RoomTypeName_Total=curs.execute(sql)
+    RoomTypeName_Data=curs.fetchall()
+#     print "data:%s"%RoomTypeName_Data
+    conn.close()
+    for i in RoomTypeName_Data:
+        NewRoomTypeid=i['Id']
+        return NewRoomTypeid
+    
+def Time(**self):
+    if self['roomTypeId']!='None':
+        self['roomTypeId']=RoomTypeName(self['roomTypeId'])
+     
+        
+    parser{'roomTypeId':"&roomTypeId=%s"%self['roomTypeId'],
+           'dateTimeType':"&dateTimeType=%s"self['dateTimeType'],
+           'queryDateTimeBegin':"&queryDateTimeBegin=%s"self['queryDateTimeBegin'],
+           'queryDateTimeEnd':"&queryDateTimeEnd=%s"self['queryDateTimeEnd']        
+           }
+    
 
 
-# def Name(url,storeid,name=None,iphone=None,orgId=None,roomTypeId=None,order=None,status=None,dateTimeType=None,queryDateTimeBegin=None,queryDateTimeEnd=None):
+# def Name(url,storeid,name=None,iphone=None,orgId=None,order=None,status=None,dateTimeType=None,queryDateTimeBegin=None,queryDateTimeEnd=None):
 def Name(**self):
-    parser={'CustomerName':"&CustomerName=%s"%self['name'],'mobile':"&mobile=%s"%self['iphone']}
+    
+
+        
+    parser={'CustomerName':"&CustomerName=%s"%self['name'],
+            'mobile':"&mobile=%s"%self['iphone'],
+            'OrgId':"&OrgId=%s"%self['OrgId'],
+            'order':"&order=%s"%self['order'],
+            'status':"&status=%s"%self['status']}
+
+        
 
     list=[]
     for key in parser:
@@ -42,7 +77,12 @@ def Name(**self):
     length=len(All_Name_data['data']['Data'])
     CommonMoudle("Search for By Name orders" , TotalAmount,length)
     
-    SQL_parser={'MainliaisonName':" and MainliaisonName='%s'"%self['name'],'MainliaisonMobile':" and MainliaisonMobile='%s'"%self['iphone']}   
+    SQL_parser={'MainliaisonName':" and MainliaisonName='%s'"%self['name'],
+                'MainliaisonMobile':" and MainliaisonMobile='%s'"%self['iphone'],
+                'OrgId':" and OrgId='%s'"%self['OrgId'],
+                'roomTypeId':" and PhysicalRoomTypeId='%s'"%self['roomTypeId'],
+                'OrderStatus':" and OrderStatus='%s'"%self['status'],
+                'OrderId':" and OrderId='%s'"%self['order']}   
 
     SQL_list=[]
     for key in SQL_parser:
@@ -59,6 +99,7 @@ def Name(**self):
     print sql_parTmp
     
     old_sql="select * from iPmsBiz.`Order` where ownerId='%s'"%self['storeid']
+#     roomTypeId_sql="select * from iPmsBiz.`Occupation` where ownerId='%s'"%self['storeid']  
     sql= old_sql + sql_parTmp
           
 #     sql=("select * from iPmsBiz.`Order` where ownerId=%s and MainliaisonName='%s' and MainliaisonMobile='%s';")%(self['storeid'],self['name'],self['iphone'])
@@ -75,15 +116,24 @@ def Name(**self):
     if TotalAmount==All_Name_Total:
         print "Search for By Name orders Amount is Right. Date:%s"%today
         for i in All_Name_Total_Data:
-            print i['MainLiaisonName']
-            name=self['name']
-            MainLiaisonName=i['MainLiaisonName']
-            if MainLiaisonName==name:
+            print i
+            name=lower(self['name'])
+            iphone=lower(self['iphone'])
+            status=lower(self['status'])
+            order=self['order']
+            
+            MainLiaisonName=lower(i['MainLiaisonName'])
+            MainliaisonMobile=lower(i['MainLiaisonMobile'])
+            OrderStatus=lower(i['OrderStatus'])
+            OrderId=i['OrderId']
+            
+
+            if MainLiaisonName==name and iphone==MainliaisonMobile and status==OrderStatus and order==OrderId:
                 #bijiao type buyizhi
-                print "Search for By Name's %s orders is Pass. Date:%s"%(MainLiaisonName,today) 
+                print "Search for By Name's %s and Phone's %s orders is Pass. Date:%s"%(MainLiaisonName,MainliaisonMobile,today) 
                 return True
             else:
-                print "Search for By Name's %s orders is Failed. Json:%s,Data:%s,Date:%s"%(MainLiaisonName,TotalAmount,All_Name_Total,today)  
+                print "Search for By Name's %s and Phone's %s orders is Failed. Json:%s,Data:%s,Date:%s"%(MainLiaisonName,MainliaisonMobile,TotalAmount,All_Name_Total,today)  
                 return False
     else:
         print "Search for By Name orders Amount is Error. Date:%s"%today
@@ -139,6 +189,6 @@ def All_Store(**self):
 if __name__ == "__main__":
     Order_Detail=All_Store(storeid=Headers['ownerid'],url=PO_Search_url)
 
-    Name(url=PO_Search_url, storeid=Headers['ownerid'],iphone="18017249012",name="wangsheng")
+    Name(url=PO_Search_url, storeid=Headers['ownerid'],iphone="18017249012",name="WangSheng",OrgId='None',order='None',status='None')
     
     
