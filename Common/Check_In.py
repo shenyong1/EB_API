c0ceea296f9fa071b82af1aff0349d07664d9cb0
Beyondhost-PC\Beyondhost <swang@beyondh.com>
2017-01-24 16:22:06 +0800

    no message

--- Common/Check_In.py
+++ Common/Check_In.py
@@ -4,249 +4,18 @@ from nose.config import flag
 from string import lower
 from random import choice
 import string
+import random
 
 def GetNumber(length=8,chars=string.letters+string.digits):
     return ''.join([choice(chars) for i in range(length)])
+
 
 def CommonMoudle(Path,data):
     if Path==data:
         return True
     else:
         return False
-
-# def CommonMoudle(name,TotalAmount,length):
-#     if TotalAmount>=10: 
-#         if length==10:
-#             print "%s JSOM_Data is Right.Date:%s"%(name,today)
-# 
-#         else:
-#             print "%s JSOM_Data is Error.Date:%s"%(name,today)
-#             return False
-#     else:
-#         print "%s JSOM_Data is %s.Date:%s"%(name,TotalAmount,today)
-        
-def RoomTypeName(RoomTypeName):
-    sql="SELECT * FROM iPms.RoomType where RoomTypeName='%s'"%RoomTypeName
-#     print sql
-    curs = conn.cursor()
-    RoomTypeName_Total=curs.execute(sql)
-    RoomTypeName_Data=curs.fetchall()
-#     print "data:%s"%RoomTypeName_Data
-    conn.close()
-    for i in RoomTypeName_Data:
-        NewRoomTypeid=i['Id']
-        return NewRoomTypeid
-    
-def Time(**self):
-    if self['roomTypeId']!='None':
-        self['roomTypeId']=RoomTypeName(self['roomTypeId'])
-     
-        
-    parser={'roomTypeId':"&roomTypeId=%s"%self['roomTypeId'],
-           'dateTimeType':"&dateTimeType=%s"%self['dateTimeType'],
-           'queryDateTimeBegin':"&queryDateTimeBegin=%s"%self['queryDateTimeBegin'],
-           'queryDateTimeEnd':"&queryDateTimeEnd=%s"%self['queryDateTimeEnd']}
-    
-    list=[]
-    for key in parser:
-        k2=parser[key]
-        k3=k2[-4:]
-        print k3
-        if k3!="None":
-            list.append(k2)
-    k4=list[0:]
-    print k4
-    parTmp=""
-    for item in k4:
-        parTmp=parTmp+item
-        
-    newurl=self['url']+parTmp
-    print newurl
-    
-    r = requests.request('GET', newurl, headers=Headers)
-    All_Name_data = json.loads(r.text)
-    TotalAmount=All_Name_data['data']['TotalCount']
-    length=len(All_Name_data['data']['Data'])
-    CommonMoudle("Search for Orders" , TotalAmount,length)  
-    
-    SQL_parser={'roomTypeId':" and PhysicalRoomTypeId='%s'"%self['roomTypeId']}   
-
-    SQL_list=[]
-    for key in SQL_parser:
-        s2=SQL_parser[key]
-        s3=s2[-6:]
-        print s3
-        if s3!="'None'":
-            SQL_list.append(s2)
-    s4=SQL_list[0:]
-    print s4
-    sql_parTmp=""
-    for item in s4:
-        sql_parTmp=sql_parTmp+item
-    print sql_parTmp
-    
-    old_sql="select * from iPmsBiz.`Occupation` where ownerId='%s'"%self['storeid']
-    
-    if self['dateTimeType']==1:
-        old_sql="select * from iPmsBiz.`Occupation` where ownerId='%s' and EstimatedDepartureTime between '%s' and '%s'"%(self['storeid'],self['queryDateTimeBegin'],self['queryDateTimeEnd'])
-    else:
-        old_sql="select * from iPmsBiz.`Occupation` where ownerId='%s' and EstimatedArriveTime between '%s' and '%s'"%(self['storeid'],self['queryDateTimeBegin'],self['queryDateTimeEnd'])
-        
-        
-    sql= old_sql + sql_parTmp
-    print sql
-    curs = conn.cursor()
-    All_Name_Total=curs.execute(sql)
-    All_Name_Total_Data=curs.fetchall()
-#     print "data:%s"%All_Name_Total
-
-
-    print TotalAmount
-    print All_Name_Total
-    
-    if TotalAmount==All_Name_Total:
-        print "Search for By orders Amount is Right. Date:%s"%today  
-    else:
-        print "Search for By orders Amount is Error. Date:%s"%today
-    
-
-
-# def Name(url,storeid,name=None,iphone=None,orgId=None,order=None,status=None,dateTimeType=None,queryDateTimeBegin=None,queryDateTimeEnd=None):
-def Name(**self):
-    
-
-        
-    parser={'CustomerName':"&CustomerName=%s"%self['name'],
-            'mobile':"&mobile=%s"%self['iphone'],
-            'OrgId':"&OrgId=%s"%self['OrgId'],
-            'order':"&order=%s"%self['order'],
-            'status':"&status=%s"%self['status']}
-
-        
-
-    list=[]
-    for key in parser:
-        k2=parser[key]
-        k3=k2[-4:]
-        print k3
-        if k3!="None":
-            list.append(k2)
-    k4=list[0:]
-    print k4
-    parTmp=""
-    for item in k4:
-        parTmp=parTmp+item
-        
-    newurl=self['url']+parTmp
-    print newurl  
-     
-    r = requests.request('GET', newurl, headers=Headers)
-    All_Name_data = json.loads(r.text)
-    TotalAmount=All_Name_data['data']['TotalCount']
-    length=len(All_Name_data['data']['Data'])
-    CommonMoudle("Search for By Name orders" , TotalAmount,length)
-    
-    SQL_parser={'MainliaisonName':" and MainliaisonName='%s'"%self['name'],
-                'MainliaisonMobile':" and MainliaisonMobile='%s'"%self['iphone'],
-                'OrgId':" and OrgId='%s'"%self['OrgId'],
-                'OrderStatus':" and OrderStatus='%s'"%self['status'],
-                'OrderId':" and OrderId='%s'"%self['order']}   
-
-    SQL_list=[]
-    for key in SQL_parser:
-        s2=SQL_parser[key]
-        s3=s2[-6:]
-        print s3
-        if s3!="'None'":
-            SQL_list.append(s2)
-    s4=SQL_list[0:]
-    print s4
-    sql_parTmp=""
-    for item in s4:
-        sql_parTmp=sql_parTmp+item
-    print sql_parTmp
-    
-    old_sql="select * from iPmsBiz.`Order` where ownerId='%s'"%self['storeid']
-    sql= old_sql + sql_parTmp
-          
-    print sql
-    curs = conn.cursor()
-    All_Name_Total=curs.execute(sql)
-    All_Name_Total_Data=curs.fetchall()
-
-#     conn.close()
-    print TotalAmount
-    print All_Name_Total
-    
-    if TotalAmount==All_Name_Total:
-        print "Search for By orders Amount is Right. Date:%s"%today
-    else:
-        print "Search for By orders Amount is Error. Date:%s"%today
-
-        
-def verify(value):
-    print value
-    if value=='':
-        newvalue={'Newvalue':"None"}
-        return newvalue
-    else:
-        newvalue={'Newvalue':value}
-        return newvalue
-    
-def All_Store(**self):
-    print self
-    r = requests.request('GET', self['url'], headers=Headers)
-    All_Store_data = json.loads(r.text)
-    TotalAmount=All_Store_data['data']['TotalCount']
-    length=len(All_Store_data['data']['Data'])
-    CommonMoudle("Search for By All Store orders" , TotalAmount,length)        
-#     print "Data:%s"%Data
-#     print "json:%s"%TotalAmount    
-    sql=("select * from iPmsBiz.`Order` where ownerId=%s;")%(self['storeid'])
-    curs = conn.cursor()
-    All_Store_Total=curs.execute(sql)
-#     print "data:%s"%All_Store_Total
-    All_Store_Total_Data=curs.fetchall()
-#     conn.close()
-    
-    if TotalAmount==All_Store_Total:
-        print "Search for By All Store orders is Pass.Date:%s"%today
-        for i in All_Store_Total_Data:
-            print i
-            name=verify(value=i['MainLiaisonName'])  
-            iphone=verify(value=i['MainLiaisonMobile'])
-            OrgId=verify(value=i['OrgId'])
-            order=verify(value=i['OrderId'])
-            status=verify(value=i['OrderStatus'])
-            
-            print name,iphone,OrgId,order,status
-            q={'name':name['Newvalue'],
-               'iphone':iphone['Newvalue'],
-               'OrgId':OrgId['Newvalue'],
-               'order':order['Newvalue'],
-               'status':status['Newvalue'],
-               'result':True}
-            print dict(q)
-            return dict(q)
-    else:
-        print "Search for By All Store orders is Failed.Json:%s,Data:%s,Date:%s"%(TotalAmount,All_Store_Total,today)
-        for i in All_Store_Total_Data:
-            print i
-            name=verify(value=i['MainLiaisonName'])  
-            iphone=verify(value=i['MainLiaisonMobile'])
-            OrgId=verify(value=i['OrgId'])
-            order=verify(value=i['OrderId'])
-            status=verify(value=i['OrderStatus'])
-            
-            q={'name':name['Newvalue'],
-               'iphone':iphone['Newvalue'],
-               'OrgId':OrgId['Newvalue'],
-               'order':order['Newvalue'],
-               'status':status['Newvalue'],
-               'result':False}
-            print dict(q)
-            return dict(q)
-
+      
 def Del_All(ownerId): 
     sql1= "delete from iPms.RoomType where ownerId=%s"%ownerId
     sql2= "delete from iPmsBiz.Rooms where ownerId=%s"%ownerId
@@ -258,6 +27,7 @@ def Del_All(ownerId):
     Del_Rooms    = curs.execute(sql2)
     Now_RoomType = curs.execute(sql3)
     Now_Rooms    = curs.execute(sql4)
+    conn.close()
     
     print "Del RoomType:%s"%Del_RoomType
     print "Del Rooms:%s"   %Del_Rooms
@@ -276,7 +46,32 @@ def BatchAdd_RoomType(**self):
             },
             {
                 "RoomNumber": self['RoomNumber1_2']
+            },
+            {
+                "RoomNumber": self['RoomNumber1_3']
+            },
+            {
+                "RoomNumber": self['RoomNumber1_4']
+            },
+            {
+                "RoomNumber": self['RoomNumber1_5']
+            },
+            {
+                "RoomNumber": self['RoomNumber1_6']
+            },
+            {
+                "RoomNumber": self['RoomNumber1_7']
+            },
+            {
+                "RoomNumber": self['RoomNumber1_8']
+            },
+            {
+                "RoomNumber": self['RoomNumber1_9']
+            },
+            {
+                "RoomNumber": self['RoomNumber1_10']
             }
+                              
                               ]
                    },
                 {
@@ -288,7 +83,32 @@ def BatchAdd_RoomType(**self):
             },
             {
                 "RoomNumber": self['RoomNumber2_2']
+            },
+            {
+                "RoomNumber": self['RoomNumber2_3']
+            },
+            {
+                "RoomNumber": self['RoomNumber2_4']
+            },
+            {
+                "RoomNumber": self['RoomNumber2_5']
+            },
+            {
+                "RoomNumber": self['RoomNumber2_6']
+            },
+            {
+                "RoomNumber": self['RoomNumber2_7']
+            },
+            {
+                "RoomNumber": self['RoomNumber2_8']
+            },
+            {
+                "RoomNumber": self['RoomNumber2_9']
+            },
+            {
+                "RoomNumber": self['RoomNumber2_10']
             }
+                            
         ]
         }]
         
@@ -336,28 +156,43 @@ if __name__ == "__main__":
 #          queryDateTimeEnd='2016-12-13'
 #          )
 
-    RoomTypeName=GetNumber(8)
-    RoomTypeName1=GetNumber(8)
-    RoomTypeName2=GetNumber(8)  
-      
-    RoomNumber=GetNumber(10)
-    RoomNumber1=GetNumber(10)
-    RoomNumber2=GetNumber(10)
-    RoomNumber3=GetNumber(10)
-    RoomNumber4=GetNumber(10)
+    RoomTypeName = random.sample(xrange(90000000), 100)
+    RoomNumber   = random.sample(xrange(80000000), 100)
+    print RoomTypeName
+    print RoomNumber
     
     Del_All(ownerId='434265567985665')
     
     RoomType=BatchAdd_RoomType(CaseNumber='TCS_001',
                           url=Search_RoomType_url,
-                          RoomTypeName1=RoomTypeName1,
                           OTARoomTypeName='Test',
                           OTARoomTypeId='Z23582JSC',
-                          RoomNumber1_1=RoomNumber1,
-                          RoomNumber1_2=RoomNumber2,
-                          RoomTypeName2=RoomTypeName2,
-                          RoomNumber2_1=RoomNumber3,
-                          RoomNumber2_2=RoomNumber4,
+                          RoomTypeName1=RoomTypeName[1],
+                          RoomNumber1_1=RoomNumber[1],
+                          RoomNumber1_2=RoomNumber[2],
+                          RoomNumber1_3=RoomNumber[3],
+                          RoomNumber1_4=RoomNumber[4],
+                          RoomNumber1_5=RoomNumber[5],                          
+                          RoomNumber1_6=RoomNumber[6],                          
+                          RoomNumber1_7=RoomNumber[7],                          
+                          RoomNumber1_8=RoomNumber[8],                          
+                          RoomNumber1_9=RoomNumber[9],                          
+                          RoomNumber1_10=RoomNumber[10],                                                    
+                          RoomTypeName2=RoomTypeName[2],
+                          RoomNumber2_1=RoomNumber[11],
+                          RoomNumber2_2=RoomNumber[12],
+                          RoomNumber2_3=RoomNumber[13],                          
+                          RoomNumber2_4=RoomNumber[14],                            
+                          RoomNumber2_5=RoomNumber[15],                            
+                          RoomNumber2_6=RoomNumber[16],                            
+                          RoomNumber2_7=RoomNumber[17],                            
+                          RoomNumber2_8=RoomNumber[18],                            
+                          RoomNumber2_9=RoomNumber[19],                            
+                          RoomNumber2_10=RoomNumber[20],                            
                           weekdayPrice='300')
+    
+
+    
+
     
     
