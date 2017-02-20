9d5493dbd94cf4209bf3af7b5b41ce8af6d76f1f
Beyondhost-PC\Beyondhost <swang@beyondh.com>
2017-02-20 17:03:35 +0800

    no message

--- TestCase/Test001.py
+++ TestCase/Test001.py
@@ -3,6 +3,8 @@ import sys
 import os
 import nose
 from nose.tools import assert_not_equal, assert_equal
+from nose.plugins.plugintest import run_buffered as run  
+from htmloutput.htmloutput import HtmlOutput 
 
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
@@ -11,7 +13,7 @@ sys.path.append(rootPath)
 from Config.EB_API_Config import *
 from Common.RoomType import *
 
-class TestNumber():
+class Test001():
     def setUp(self):
         print "Test Start"
 
@@ -88,10 +90,18 @@ class TestNumber():
         Result = Serach['Result']
 #         if (Result != True):
 #             return Result        
-        assert_equal(Result,True)
+        assert_not_equal(Result,True)
                 
-    def TestDown(self):
+    def tearDown(self):
         print "Test End"
+#         path= os.path.dirname(__file__)  
+#         print path
+#         outfile = os.path.join(path, "result.html")  
+#         print outfile
+#         run(argv=['nosetests', '-v','--with-html-output','--html-out-file=result.html'],plugins=[HtmlOutput()])
+        
+# if __name__ == "__main__":
+#     TestNumber()
 
 
 
